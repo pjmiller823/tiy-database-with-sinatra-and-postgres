a509ac680922958c182cbd7c80e0ee11f478a107
Patrick Miller <pjmiller823@gmail.com>
2017-03-30 14:59:39 -0400

    set up plan for the project.

--- tiydatabase.rb
+++ tiydatabase.rb
@@ -5,3 +5,12 @@ require 'sinatra/reloader' if development?
 get '/' do
   erb :home
 end
+
+# Disign webpage with: HOME PAGE, Nav bar showing EMPLOYEES (clickable),
+# employees links to NEW EMPLOYEE page and SEARCH
+# nice format for stuff
+# Show page must be editable
+
+# Make and populate database. (made, add some inputs to see in EMPLOYEES)
+# Create a home page and make it look nice
+# Make pages for home, employee index, employee show, employee new.
